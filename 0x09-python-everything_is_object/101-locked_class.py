b28e9fd34cd420dcc96fe60205116f6983b3fab6
EdithM5 <ewmwenje@gmail.com>
2023-04-04 21:01:53 +0100

    task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Define the class"""
+
 
 class LockedClass:
+
+    """
+    prevents user from creating new attributes except
+    for first name
+    """
     __slots__ = ["first_name"]
-
-
