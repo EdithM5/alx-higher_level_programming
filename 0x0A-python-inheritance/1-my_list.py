a3e0db725219d5f906631b4aebfda6fb1c96d83e
EdithM5 <ewmwenje@gmail.com>
2023-04-12 15:08:52 +0100

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" defining a class that will inherit from list"""
+""" 
+defining a class that will inherit from list
+"""
 
 
 class MyList(list):
     """defining a fuction that prints a sorted int list"""
+
     def print_sorted(self):
-        sorted_list = sorted(self)
         """prints sorted list"""
-        print(sorted_list)
+        print(sorted(self))
