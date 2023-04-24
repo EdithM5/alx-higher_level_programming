6770cfe22d380a3953b760c2ac1e219fe1fadc9d
EdithM5 <ewmwenje@gmail.com>
2023-04-24 09:54:08 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" defining the function to read file"""
+
+
+def read_file(filename=""):
+    """
+    this function reads a text file
+    and prints it to stdout
+    """
+    with open(filename, 'r', encoding="utf-8") as f
+    print(f.read(), end="")
