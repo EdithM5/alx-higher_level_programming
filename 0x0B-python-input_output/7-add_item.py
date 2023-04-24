c2e3c11e6f4e3471121eab92285e519c64529968
EdithM5 <ewmwenje@gmail.com>
2023-04-24 15:22:26 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ defining the function that adds to a pythos list and saves"""
 import sys
+from os import path
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 
-if __name__ == "__main__":
-    load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
-    save_to_json_file =  \
-            __import__('5-save_to_json_file').save_to_json_file
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+filename = "add_item.json"
+
+if not path.exists(filename):
+    items = []
+else:
+    items = load_from_json_file(filename)
+
+for arg in sys.argv[1:]:
+    items.append(arg)
+
+save_to_json_file(items, filename)
