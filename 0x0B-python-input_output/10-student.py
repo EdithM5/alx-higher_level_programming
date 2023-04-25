8513f105026a4eba206574fb2db0b41029a1867d
EdithM5 <ewmwenje@gmail.com>
2023-04-25 08:31:14 +0100

    task 11

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""defining a class that defines a student"""
+
+
+class Student:
+    """
+    represent a student using 3 attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialise a new student
+        @first_name
+        @last_name
+        @age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        get a dict representation of student
+        If attrs is a list of strings,
+        only attribute names contained in this list
+        must be retrieved
+        Otherwise, all attributes must be retrieved
+        """
+        if (type(attrs) == list and
+                all(type(i) == str for i in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
